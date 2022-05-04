0522800e2ab6ac053f252b00743047cd678257e6
dybeca123 <103453892+dybeca123@users.noreply.github.com>
2022-05-04 16:47:53 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,30 @@
+//1-a
+console.log(20 + 20 *30);
+
+//1-b
+console.log(4**2 / 30);
+
+//1-c 
+console.log(9**4 + 2)* 6 - 1
+
+//2
+console.log(10%3*10**1+1-10*4/2);
+
+//3
+console.log('Rebeca')
+
+//4
+const x = 3;
+const y = 5;
+console.log(2*x*3*y)
+
+//5
+const d = 2
+const e = 3
+const f = 4
+console.log(d+e+f)
+
+//6
+const salario = 1800;
+const dobra = 2;
+console.log(salario*dobra);
